a4928037b095f5e0cf09aa76986b8a984c8667ab
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-11-21 23:24:42 +0100

    Added Photo Uploader Carrierwave & Cloudinary

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,11 @@
 class Cocktail < ApplicationRecord
+
+
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   has_many :reviews
 
   validates :name, presence: true, uniqueness: true
+
+  mount_uploader :photo, PhotoUploader
 end
